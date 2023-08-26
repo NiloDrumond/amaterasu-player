dd3033192df652fb26158d4e47936200b067624f
Nilo Drumond <nilobdrumond@gmail.com>
2023-08-26 13:32:47 -0300

    wip

--- src/lib/components/search-input.svelte
+++ src/lib/components/search-input.svelte
@@ -26,7 +26,7 @@
     bind:value={inputValue}
     on:input={handleInput}
     type="search"
-    class="peer bg-slate-200 border border-slate-400 pl-10 placeholder-primary focus:placeholder:opacity-0 focus:border-crystal rounded-lg py-2 px-4 transition-colors focus-visible:outline-none dark:bg-gray-700 dark:border-gray-950"
+    class="peer bg-slate-200 border border-slate-400 pl-10 placeholder-primary focus:placeholder:opacity-0 focus:border-crystal rounded-lg py-2 px-4 transition-colors focus-visible:outline-none dark:bg-gray-700 dark:border-gray-950 dark:hover:border-crystal"
     placeholder="Search..."
   />
   <div class="absolute left-3 top-3 peer-focus:text-crystal">
